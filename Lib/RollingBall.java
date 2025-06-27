9807b2afb0c4d78480b1eb8c9b64d3c725a11503
Nannapat-p <nannapat.pak@ku.th>
2025-06-27 15:39:35 +0700

    Add files via upload

--- Lib/RollingBall.java
+++ Lib/RollingBall.java
@@ -1,17 +1,30 @@
 package Lib;
 
-//import java.awt.Color;
-import java.awt.Graphics;
-
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
 import javax.swing.JPanel;
 
-public class RollingBall extends JPanel {
+public class RollingBall extends JPanel implements ActionListener {
+
+    int x = 10,y=90,start = 0;
+    
+    public RollingBall(){
+        Timer time1 = new Timer(30,this);
+        time1.start();
+    }
     public void paintComponent(Graphics g){
         super.paintComponent(g);
-        g.drawOval(100, 90, 100, 100);
-       // g.setColor(Color.red);
-        g.fillArc(100, 90, 100, 100, 0, 180);
-        
-        
+        g.drawOval(x, y, 100, 100);
+        g.fillArc(x, y, 100, 100, start, 180);
     }
+
+    public void actionPerformed(ActionEvent e) {
+        x  -= 5;
+        start +=5;
+       if (x == -90) {
+            x = 300;
+        }
+        repaint();
+        }
 }
